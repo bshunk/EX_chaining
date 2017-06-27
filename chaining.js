f8057de4d29352214ddfbfddfa5a601988968a85
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-27 16:16:09 -0500

    finsih chaining JS

--- chaining.js
+++ chaining.js
@@ -1,17 +1,1 @@
-console.log("Ahoy, captain!");
-
-var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
-
-integers.sort(function(a, b) {return a - b}); console.log(integers.sort(function(a, b) {return a - b}));
-
-
-// Using one single line of JavaScript code, complete the following tasks on the array of integers below:
-
-// Sort the numbers in descending order (10, 9, 8, 7, etc).
-// Remove any integers greater than 19.
-// Multiply each remaining number by 1.5 and then subtract 1.
-// Then output (either in the DOM or the console) the sum of all the resulting numbers.
-// var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
-
-
-
+console.log("Numbers = ", [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8].sort(function(a, b){return b - a}).filter(function(filtered){return filtered < 19}).map(function(mapped){return mapped * 1.5 - 1}).reduce(function(x, y){return x + y}));
